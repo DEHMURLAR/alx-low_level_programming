7785395d2aa4b6fde69fc6dc23bd6499ca3a75ee
DEHMURLAR <light4deen@gmail.com>
2022-12-29 10:37:52 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"`
+#include "main.h"
 
 /**
  * print_sign - print the sign of a num
@@ -10,19 +10,19 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        else if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+        else
+        {
+                _putchar('-');
+                return (-1);
+        }
 }
