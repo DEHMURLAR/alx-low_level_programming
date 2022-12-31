e5fe878def8cd973b9163a3090f34bad3cd30cf0
DEHMURLAR <light4deen@gmail.com>
2022-12-31 14:03:54 -0800

    Task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_numbers - print 0123456789
-* Return: void
+*print_numbers - prin t 0123456789
+*Return: 0
 */
 
 void print_numbers(void)
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
